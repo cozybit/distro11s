9f478d179233785763058d9ec833a4d394d39c80
Brian Cavagnolo <brian@cozybit.com>
2011-07-22 15:48:57 -0700

    move hwsim_tests out of /root
    In order for key-based ssh login to work, /root and /root/.ssh
    have to have the correct permissions.  We don't want to have to
    install hwsim_tests as root.  So move them somewhere else.

--- scripts/hwsim_tests.sh
+++ scripts/hwsim_tests.sh
@@ -3,6 +3,6 @@
 source `dirname $0`/common.sh
 
 Q pushd ${DISTRO11S_SRC}/hwsim_tests || exit 1
-mkdir -p ${STAGING}/root/hwsim_tests/ || exit 1
-cp -r * ${STAGING}/root/hwsim_tests/ || exit 1
+mkdir -p ${STAGING}/usr/local/share/hwsim_tests/ || exit 1
+cp -r * ${STAGING}/usr/local/share/hwsim_tests/ || exit 1
 exit 0
