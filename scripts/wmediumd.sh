7566752b4bd013f818309aafe9f75d0c5f36141d
Brian Cavagnolo <brian@cozybit.com>
2011-07-25 17:51:13 -0700

    fix build bug in wmediumd

--- scripts/wmediumd.sh
+++ scripts/wmediumd.sh
@@ -4,6 +4,6 @@ source `dirname $0`/common.sh
 
 Q pushd ${DISTRO11S_SRC}/wmediumd || exit 1
 export CFLAGS="${CFLAGS} -D_GNU_SOURCE"
-export SUBDIRS="rawsocket gwmediumd" 
+export SUBDIRS="rawsocket wmediumd"
 do_stamp_cmd wmediumd.make make
 do_stamp_cmd wmediumd.install cp ./wmediumd/wmediumd ${STAGING}/usr/local/bin
