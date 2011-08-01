d91f6ab1ab2fe03b0685a2ccb05533d1b5cb5779
Thomas Pedersen <thomas@cozybit.com>
2011-08-01 12:02:39 -0700

    add hostap + hostapd package and build script

---
+++ scripts/hostap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# make package manager happy with just fetching sources
+exit 0
---
+++ scripts/hostapd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source `dirname $0`/common.sh
+
+Q pushd ${DISTRO11S_SRC}/hostap/hostapd || exit 1
+
+CONFIG=${TOP}/board/${DISTRO11S_BOARD}/${DISTRO11S_BOARD}_hostapd.config
+if [ ! -e ${CONFIG} ]; then
+	echo "No config for hostapd in ${CONFIG}, using default"
+	CONFIG=defconfig
+fi
+do_stamp_cmd hostapd.config cp ${CONFIG} ./.config && echo "CONFIG_LIBNL20=y" >> ./.config
+do_stamp_cmd hostapd.make PREFIX=${STAGING}/usr/local make -j ${DISTRO11S_JOBS}
+do_stamp_cmd hostapd.install DESTDIR=${STAGING} make install && cp hostapd.conf ${STAGING}/etc/
