a21eeea5bf1b3ad5548c7f4d5e0ca13d6c77b601
Guillermo Nunez <guillermo@cozybit.com>
2011-08-17 12:32:04 -0700

    Add support for clean shutdown when the power button is pressed

--- scripts/zotac-cleanups.sh
+++ scripts/zotac-cleanups.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 source `dirname $0`/common.sh
+
+#Load ath9k module with nohwcrypt parametar at boot time
 echo "options ath9k nohwcrypt=1" >> ${STAGING}/etc/modprobe.d/local
+
+#Install & configure acpid to enable clean shutdown when pressing the power
+#button
+sudo mount proc ${STAGING}/proc/ -t proc
+sudo chroot ${STAGING} apt-get -y --force-yes install acpid
+sudo umount ${STAGING}/proc/
