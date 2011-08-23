c6147c26309ee8da1e155657cba06b6459bf3360
Thomas Pedersen <thomas@cozybit.com>
2011-08-22 18:00:28 -0700

    specify a range of nodes to sync

--- scripts/foreach.sh
+++ scripts/foreach.sh
@@ -7,7 +7,15 @@ if [ "${DISTRO11S_NUM_NODES}" = "" ]; then
 	exit 1
 fi
 
-for n in `seq 0 $((${DISTRO11S_NUM_NODES} - 1))`; do
+if [ "${DISTRO11S_START_NODE}" = "" ]; then
+	DISTRO11S_START_NODE=0
+fi
+
+if [ "${DISTRO11S_END_NODE}" = "" ]; then
+	DISTRO11S_END_NODE=$((${DISTRO11S_NUM_NODES} - 1))
+fi
+
+for n in `seq ${DISTRO11S_START_NODE} ${DISTRO11S_END_NODE}`; do
 	SUFFIX=$((`echo ${DISTRO11S_STATIC_IP} | cut -d '.' -f 4` + ${n}))
 	export IP=`echo ${DISTRO11S_STATIC_IP} | cut -d '.' -f 1-3`".${SUFFIX}"
 	export HOSTNAME=${DISTRO11S_HOSTNAME}-${n}
