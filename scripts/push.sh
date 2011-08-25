a6b553572e0888a7fb1b00d701d40799c50880a8
Guillermo Nunez <guillermo@cozybit.com>
2011-08-24 21:27:16 -0700

    Add support to push the files related to a particular package only

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,6 +2,7 @@
 
 source `dirname $0`/common.sh
 
+PACKAGE=all
 DESTINATION="/"
 # HOSTNAME set by foreach.sh
 # don't push to ourselves
@@ -17,27 +18,39 @@ push.sh: push distro11s to a deployed system using rsync
 
 -d <destination> Destination directory on the device (default is
                  ${DESTINATION})
+-p <package_name> It will push only the files related to that package
 
 EOF
 )"
 }
 
-while getopts "hu:d:b:" opt; do
+# translate long options to short
+for arg
+do
+        delim=""
+        case "$arg" in
+                --help) args="${args}-h ";;
+		--board) args="${args}";;
+                --destination) args="${args}-d ";;
+		--package) args="${args}-p ";;
+                # pass through anything else
+                *) [[ "${arg:0:1}" == "--" ]] || delim="\""
+                args="${args}${delim}${arg}${delim} ";;
+        esac
+done
+# reset the translated args
+eval set -- $args
+
+while getopts "hu:d:b:p:" opt; do
 	case $opt in
-		h)
-			print_help
-			exit 0
-			;;
-		u)
-			U=${OPTARG}
-			;;
-		d)
-			DESTINATION=${OPTARG}
-			;;
-		\?)
-			echo "Invalid option: -$OPTARG" >&2
-			exit 1
-			;;
+		b)	BOARD=${OPTARG};;
+		h)	print_help
+			exit 0;;
+		u)	U=${OPTARG};;
+		d)	DESTINATION=${OPTARG};;
+		p)	PACKAGE=${OPTARG};;
+		\?)	echo "Invalid option: -${OPTARG}" >&2
+			exit 1;;
 	esac
 done
 
@@ -59,5 +72,32 @@ fi
 # NB: trailing slash is important!
 SRC=${STAGING}/
 DEST=${U}:${DESTINATION}
-echo "About to push ${SRC} to ${DEST}"
-sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync --exclude='*root/.ssh/*' -av ${SRC} ${DEST} || exit 1
+
+if [ "${PACKAGE}" == "kernel" ]; then
+	echo "About to push the kernel"
+	sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync -av ${SRC}boot/bzImage ${DEST}boot/bzImage || exit 1
+	echo "About to push the modules"
+	sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync -av ${SRC}lib/modules/ ${DEST}lib/modules/ || exit 1
+	if [ -d ${SRC}lib64/modules  ]; then
+		sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync -av ${SRC}lib64/modules/ ${DEST}lib64/modules/ || exit 1
+	fi
+
+elif [ "${PACKAGE}" == "iw" || "${PACKAGE}" == "authsae" || "${PACKAGE}" == "libnl" || "${PACKAGE}" == "meshkit" || "${PACKAGE}" == "wmediumd" ]; then
+	echo "About to push ${PACKAGE}: From ${SRC}usr/local/ to ${DEST}usr/local/"
+	sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync -av ${SRC}usr/local/ ${DEST}usr/local/ || exit 1
+
+	if [ "${PACKAGE}" == "meshkit" ]; then
+		sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync -av ${SRC}etc/ ${DEST}etc/ || exit 1
+	elif [ "${PACKAGE}" == "wmediumd" ]; then
+		sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync -av ${SRC}etc/wmediumd/ ${DEST}etc/wmediumd/ || exit 1
+	fi
+
+else
+	echo "Package not supported yet. Push all."
+	exit 1
+fi
+
+if [ "${PACKAGE}" == "all" ]; then
+	echo "About to push ${SRC} to ${DEST}"
+	sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync --exclude='*root/.ssh/*' -av ${SRC} ${DEST} || exit 1
+fi
