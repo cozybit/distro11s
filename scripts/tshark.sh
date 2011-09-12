760bcb94ed7bdc929bf13d1d7923ec8aafb8394d
Thomas Pedersen <thomas@cozybit.com>
2011-09-12 12:06:36 -0700

    qemu needs tshark for tests
    You might have to chroot into qemu and do an 'apt-get update' in order
    for this to work. These are .debs built from SVN which include Brian's
    (RIP) mesh patches.

---
+++ scripts/tshark.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source `dirname $0`/common.sh
+
+if [ ! -d $PWD/packages/tshark ]; then
+	echo "No tshark package."
+	exit 1
+fi
+
+if [ ! -e ${STAMPS}/tshark.stamp -o ${FORCE_BUILD} -eq 1 ]; then
+	Q pushd $PWD/packages/tshark
+	cp *.deb ${STAGING}/tmp/
+	# install wireshark_common and have it fail...
+	sudo chroot ${STAGING} dpkg -i /tmp/wireshark-common_1.7.0_i386.deb
+	# now tell apt-get to install the missing dependencies
+	sudo chroot ${STAGING} apt-get -f install
+	sudo chroot ${STAGING} dpkg -i /tmp/tshark_1.7.0_i386.deb && touch ${STAMPS}/tshark.stamp
+fi
