2f3282209386d2142f13a04092f34a864c89459f
Guillermo Nunez <guillermo@cozybit.com>
2011-09-19 10:21:42 -0700

    Add 64bit tshark packages and update its script

--- scripts/tshark.sh
+++ scripts/tshark.sh
@@ -11,9 +11,13 @@ if [ ! -e ${STAMPS}/tshark.stamp -o ${FORCE_BUILD} -eq 1 ]; then
 	Q pushd $PWD/packages/tshark
 	cp *.deb ${STAGING}/tmp/
 	# install wireshark_common and have it fail...
-	sudo chroot ${STAGING} dpkg --force-architecture -i /tmp/wireshark-common_1.7.0_i386.deb
+	ARCH="i386"
+	[ "`uname -m`" == "x86_64" ] && ARCH="amd64"
+
+	sudo chroot ${STAGING} apt-get update
+	sudo chroot ${STAGING} dpkg -i /tmp/wireshark-common_1.7.0_${ARCH}.deb
 	# now tell apt-get to install the missing dependencies
 	sudo chroot ${STAGING} apt-get -f install
-	sudo chroot ${STAGING} dpkg --force-architecture -i /tmp/tshark_1.7.0_i386.deb && touch ${STAMPS}/tshark.stamp
+	sudo chroot ${STAGING} dpkg -i /tmp/tshark_1.7.0_${ARCH}.deb && touch ${STAMPS}/tshark.stamp
 	sudo rm ${STAGING}/tmp/*.deb
 fi
