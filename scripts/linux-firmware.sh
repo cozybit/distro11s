27391ef8eaf44996d5c97ac239288af9fc954a49
Thomas Pedersen <thomas@cozybit.com>
2011-09-23 15:49:44 -0700

    add linux-firmware tree to zotac build

---
+++ scripts/linux-firmware.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+source `dirname $0`/common.sh
+
+Q pushd ${DISTRO11S_SRC}/linux-firmware || exit 1
+mkdir -p ${STAGING}/lib/firmware
+do_stamp_cmd linux-firmware.install cp -R * ${STAGING}/lib/firmware 
