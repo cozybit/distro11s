42698ac0c0807b6c9c901b3b19ff3f36d2e7d95a
Javier Cardona <javier@cozybit.com>
2011-09-26 14:53:38 -0700

    Link iw and wmediumd against the libs in staging, not the local ones

--- scripts/wmediumd.sh
+++ scripts/wmediumd.sh
@@ -4,6 +4,7 @@ source `dirname $0`/common.sh
 
 Q pushd ${DISTRO11S_SRC}/wmediumd || exit 1
 export CFLAGS="${CFLAGS} -D_GNU_SOURCE"
+export LDFLAGS="-L ${STAGING}/usr/local/lib"
 export SUBDIRS="rawsocket wmediumd"
 do_stamp_cmd wmediumd.make make
 do_stamp_cmd wmediumd.install cp ./wmediumd/wmediumd ${STAGING}/usr/local/bin
