ea3afb36e29e8922aed96f8bb7ce173e3f09e31d
Thomas Pedersen <thomas@cozybit.com>
2011-09-29 10:34:23 -0700

    fix iw.sh

--- scripts/iw.sh
+++ scripts/iw.sh
@@ -4,10 +4,7 @@ source `dirname $0`/common.sh
 
 Q pushd ${DISTRO11S_SRC}/iw || exit 1
 
-# iw Makefile fixup
-sed -i -e 's/^LIBS += -lnl-genl$/LIBS += -lnl-genl-3/' ${DISTRO11S_SRC}/iw/Makefile
-
 do_stamp_cmd iw.make make \
-	PKG_CONFIG_LIBDIR=${STAGING}/usr/local/lib/pkgconfig \
-	PREFIX=${STAGING}/usr/local \
+	PKG_CONFIG_LIBDIR=${STAGING}/usr/local/lib/pkgconfig/ \
+	PREFIX=${STAGING}/usr/local/ \
 	V=1 install
