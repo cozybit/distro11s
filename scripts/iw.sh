7651cf4dbf3d06fc1807c5b89991d4c062a2e04f
Thomas Pedersen <thomas@cozybit.com>
2011-09-30 11:04:06 -0700

    more iw build hacks, libnl had a new minor version which breaks the build..

--- scripts/iw.sh
+++ scripts/iw.sh
@@ -4,7 +4,10 @@ source `dirname $0`/common.sh
 
 Q pushd ${DISTRO11S_SRC}/iw || exit 1
 
+# iw Makefile fixup
+sed -i -e 's/libnl-3.0/libnl-3.1/' ${DISTRO11S_SRC}/iw/Makefile
+
 do_stamp_cmd iw.make make \
-	PKG_CONFIG_LIBDIR=${STAGING}/usr/local/lib/pkgconfig/ \
+	PKG_CONFIG_PATH=${STAGING}/usr/local/lib/pkgconfig/ \
 	PREFIX=${STAGING}/usr/local/ \
 	V=1 install
