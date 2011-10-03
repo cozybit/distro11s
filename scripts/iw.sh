a82f6c69ec19e0611840f207ed8641ca3cf290e7
Guillermo Nunez <guillermo@cozybit.com>
2011-10-03 16:22:44 -0700

    Remove iw hack to build against libnl 3.1.
    Now libnl3.0 is fetched by default, so iw's Makefile hack is not needed

--- scripts/iw.sh
+++ scripts/iw.sh
@@ -4,9 +4,6 @@ source `dirname $0`/common.sh
 
 Q pushd ${DISTRO11S_SRC}/iw || exit 1
 
-# iw Makefile fixup
-sed -i -e 's/libnl-3.0/libnl-3.1/' ${DISTRO11S_SRC}/iw/Makefile
-
 do_stamp_cmd iw.make make \
 	PKG_CONFIG_PATH=${STAGING}/usr/local/lib/pkgconfig/ \
 	PREFIX=${STAGING}/usr/local/ \
