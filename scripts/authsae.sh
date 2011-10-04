1c388e66c1f71b9bb1791993d9f5cb89c88ba0ef
Guillermo Nunez <guillermo@cozybit.com>
2011-10-04 10:54:51 -0700

    Change authsae Makefile to use the default version of libnl

--- scripts/authsae.sh
+++ scripts/authsae.sh
@@ -2,6 +2,9 @@
 
 source `dirname $0`/common.sh
 
+sed -i -e 's/-lnl-3/-lnl/' ${DISTRO11S_SRC}/authsae/linux/Makefile
+sed -i -e 's/-lnl-genl-3/-lnl-genl/' ${DISTRO11S_SRC}/authsae/linux/Makefile
+
 Q pushd ${DISTRO11S_SRC}/authsae || exit 1
 export CFLAGS="${CFLAGS} -D_GNU_SOURCE"
 do_stamp_cmd authsae.make make clean; PREFIX=${STAGING}/usr/local make -C linux install;
