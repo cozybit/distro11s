e21e195f426083bc0f1ccc8e7dc69f18eacdb21e
Guillermo Nunez <guillermo@cozybit.com>
2011-10-05 15:29:16 -0700

    Avoid building libnl with multiple jobs

--- scripts/libnl.sh
+++ scripts/libnl.sh
@@ -5,5 +5,5 @@ source `dirname $0`/common.sh
 Q pushd ${DISTRO11S_SRC}/libnl || exit 1
 do_stamp_cmd libnl.autogen ./autogen.sh
 do_stamp_cmd libnl.configure ./configure --prefix=${STAGING}/usr/local
-do_stamp_cmd libnl.make "make clean; make -j ${DISTRO11S_JOBS};"
+do_stamp_cmd libnl.make "make clean; make;"
 do_stamp_cmd libnl.install make install
