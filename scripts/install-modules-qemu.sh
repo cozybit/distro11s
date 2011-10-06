518b480268400e86c296bf2f079fc4e92321ccc3
Guillermo Nunez <guillermo@cozybit.com>
2011-10-06 14:23:31 -0700

    Use distro11s.conf variables.
    Also check if qemu is running

--- scripts/install-modules-qemu.sh
+++ scripts/install-modules-qemu.sh
@@ -3,17 +3,16 @@
 . scripts/common.sh
 
 LOOPDEV=/dev/loop0
-MNTPOINT=/mnt/qemu-rootfs
-IMGFILE=${PWD}/out/qemu/rootfs.ext3
+MNTPOINT=`sudo mktemp -d --tmpdir=/mnt`
+IMGFILE=${DISTRO11S_OUT}/qemu/rootfs.ext3
 
 root_check "This script mounts the loop interface and copies kernel modules to it"
-sudo killall qemu
-sudo killall qemu-system-x86_64
+
+QEMU_RUNNING=`ps aux | grep -c 'qemu/bzImage'`
+[ "${TEST}" == "y" -a  ${QEMU_RUNNING} -gt 1 ] && { echo "qemu is running! Please, halt it."; exit 1; }
+
 sudo losetup $LOOPDEV || sudo losetup -s $LOOPDEV $IMGFILE
 sudo mount -t ext3 $LOOPDEV $MNTPOINT
-pushd src/kernel 
+cd ${DISTRO11S_SRC}/kernel 
 sudo INSTALL_MOD_PATH=$MNTPOINT make modules_install
-popd
-# Also copy the kernel to the place where qemu.sh expects it
-sudo cp ${PWD}/src/kernel/arch/x86/boot/bzImage ${PWD}/out/qemu
 sudo umount $MNTPOINT
