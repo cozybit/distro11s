befe4d35ab32394d47e789126050604fda093279
Thomas Pedersen <thomas@cozybit.com>
2011-10-20 21:09:36 -0700

    add git HEAD SHA-1 to kernel extraversion

--- scripts/kernel.sh
+++ scripts/kernel.sh
@@ -23,8 +23,16 @@ if [ ! -e ${CONFIG} ]; then
     echo "No config for kernel.  Expected ${CONFIG}."
     exit 1
 fi
+
+# set kernel extraversion to our git HEAD SHA-1
+HEAD_SHA=`git log --oneline -n1 | awk '{print $1}'`
+sed -i "s/^\(EXTRAVERSION = -rc[[:digit:]]*\).*$/\1-${HEAD_SHA}/" Makefile
+
 do_stamp_cmd kernel.config cp ${CONFIG} ./.config
 do_stamp_cmd kernel.oldconfig "yes \"\" | make oldconfig"
 do_stamp_cmd kernel.make make -j ${DISTRO11S_JOBS}
 do_stamp_cmd kernel.install 'cp ${BOARD11S_KERNEL} ${DISTRO11S_OUT}/${DISTRO11S_BOARD}; cp ${BOARD11S_KERNEL} ${STAGING}/boot;'
 INSTALL_MOD_PATH=${STAGING} do_stamp_cmd kernel.modules make -j ${DISTRO11S_JOBS} modules_install
+
+# revert our Makefile change to make git happy
+git checkout Makefile
