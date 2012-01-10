e13879a4eb0ef80c72f2ea7df3fe4586b51840f0
Guillermo Nunez <guillermo@cozybit.com>
2012-01-10 23:07:31 +0100

    Pass both make commands to do_stamp_cmd.

--- scripts/authsae.sh
+++ scripts/authsae.sh
@@ -7,6 +7,6 @@ sed -i -e 's/-lnl-genl-3/-lnl-genl/' ${DISTRO11S_SRC}/authsae/linux/Makefile
 
 Q pushd ${DISTRO11S_SRC}/authsae || exit 1
 export CFLAGS="${CFLAGS} -D_GNU_SOURCE"
-do_stamp_cmd authsae.make make clean; PREFIX=${STAGING}/usr/local make -C linux install;
+do_stamp_cmd authsae.make "make clean; PREFIX=${STAGING}/usr/local make -C linux install;"
 mkdir -p ${STAGING}/usr/local/share/authsae/ || exit 1
 do_stamp_cmd authsae.files cp -r ./config ${STAGING}/usr/local/share/authsae/
