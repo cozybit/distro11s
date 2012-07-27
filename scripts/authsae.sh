d9abc5e538a09a37ff3d08e6435a643312aba9ab
Jason Abele <jason@cozybit.com>
2012-07-27 11:08:16 -0700

    Changes to support authsae build with cmake
    Uses latest libnl
    	(Note 3.2.10 broke iw, fixed in 3.2.11)

--- scripts/authsae.sh
+++ scripts/authsae.sh
@@ -2,15 +2,12 @@
 
 source `dirname $0`/common.sh
 
-sed -i -e 's/-lnl-3/-lnl/' ${DISTRO11S_SRC}/authsae/linux/Makefile
-sed -i -e 's/-lnl-genl-3/-lnl-genl/' ${DISTRO11S_SRC}/authsae/linux/Makefile
-
 sudo mkdir ${STAGING}/src
 sudo mount --bind ${DISTRO11S_SRC} ${STAGING}/src
 
 #Q pushd ${DISTRO11S_SRC}/authsae || exit 1
 
-echo "cd /src/authsae; export CFLAGS=\"${CFLAGS} -D_GNU_SOURCE\"; make clean; PREFIX=/usr/local make -C linux install;" > ${STAGING}/authsae.sh
+echo "cd /src/authsae; rm -rf build; mkdir build; cd build; export CFLAGS=\"${CFLAGS} -D_GNU_SOURCE\"; cmake -DCMAKE_INSTALL_PREFIX=/usr/local ../; make install;" > ${STAGING}/authsae.sh
 chmod +x ${STAGING}/authsae.sh
 do_stamp_cmd authsae.make sudo chroot ${STAGING} /authsae.sh
 mkdir -p ${STAGING}/usr/local/share/authsae/ || exit 1
