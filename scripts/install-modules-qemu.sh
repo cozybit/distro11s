bdf287a618a1aa36b7d82b7e215e84a1fb6d6a25
Ashok Nagarajan <ashok@cozybit.com>
2012-08-01 14:15:56 -0700

    Revert to old way of getting loop device
    The generic method of finding a loop device doesn't seem to work well with
    install-modules.qemu.sh.

--- scripts/install-modules-qemu.sh
+++ scripts/install-modules-qemu.sh
@@ -2,8 +2,8 @@
 
 source `dirname $0`/common.sh
 
-LOOPDEV=`get_loop_dev`
-#LOOPDEV=/dev/loop0
+#LOOPDEV=`get_loop_dev`
+LOOPDEV=/dev/loop0
 MNTPOINT=`sudo mktemp -d --tmpdir=/mnt`
 IMGFILE=${DISTRO11S_OUT}/qemu/rootfs.ext3
 
