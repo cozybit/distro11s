d792d6dd00eff63916f1fc085cf82642f6a17cc3
Jason Abele <jason@cozybit.com>
2012-08-08 11:09:40 -0700

    Update to make authsae.cfg install in /etc

--- scripts/authsae.sh
+++ scripts/authsae.sh
@@ -7,7 +7,7 @@ sudo mount --bind ${DISTRO11S_SRC} ${STAGING}/src
 
 #Q pushd ${DISTRO11S_SRC}/authsae || exit 1
 
-echo "cd /src/authsae; rm -rf build; mkdir build; cd build; export CFLAGS=\"${CFLAGS} -D_GNU_SOURCE\"; cmake -DCMAKE_INSTALL_PREFIX=/usr/local ../; make install;" > ${STAGING}/authsae.sh
+echo "cd /src/authsae; rm -rf build; mkdir build; cd build; export CFLAGS=\"${CFLAGS} -D_GNU_SOURCE\"; cmake -DSYSCONF_INSTALL_DIR=/etc -DCMAKE_INSTALL_PREFIX=/usr/local ../; make install;" > ${STAGING}/authsae.sh
 chmod +x ${STAGING}/authsae.sh
 do_stamp_cmd authsae.make sudo chroot ${STAGING} /authsae.sh
 mkdir -p ${STAGING}/usr/local/share/authsae/ || exit 1
