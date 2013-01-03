87643019a3826dcf01a529bdd32a4c709c1ab902
Bob Copeland <me@bobcopeland.com>
2013-01-02 21:42:13 -0500

    distro11s: start using libnl 3.2
    This fixes a build problem for hostapd on zotacs for me, as well as
    segfaults in virtually everything due to some broken commits on libnl
    HEAD.
    
    Signed-off-by: Bob Copeland <me@bobcopeland.com>

--- scripts/hostapd.sh
+++ scripts/hostapd.sh
@@ -9,6 +9,6 @@ if [ ! -e ${CONFIG} ]; then
 	echo "No config for hostapd in ${CONFIG}, using default"
 	CONFIG=defconfig
 fi
-do_stamp_cmd hostapd.config cp ${CONFIG} ./.config && echo "CONFIG_LIBNL20=y" >> ./.config
+do_stamp_cmd hostapd.config cp ${CONFIG} ./.config && echo "CONFIG_LIBNL32=y" >> ./.config
 do_stamp_cmd hostapd.make "make clean; PREFIX=${STAGING}/usr/local make -j ${DISTRO11S_JOBS};"
 do_stamp_cmd hostapd.install DESTDIR=${STAGING} make install && cp hostapd.conf ${STAGING}/etc/
