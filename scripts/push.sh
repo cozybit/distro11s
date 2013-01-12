e459904768fa0d4ad539ca5386aa795387c4b7ae
Thomas Pedersen <thomas@cozybit.com>
2013-01-12 15:32:04 -0800

    distro11s: don't sync root's home

--- scripts/push.sh
+++ scripts/push.sh
@@ -97,5 +97,5 @@ fi
 
 if [ "${PACKAGE}" == "all" ]; then
 	echo "About to push ${SRC} to ${DEST}"
-	sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync --exclude='*root/.ssh/*' --exclude='dev' --exclude='proc' --exclude='var/log' --exclude='mnt' --exclude='var/cache' --exclude='etc' -av ${SRC} ${DEST} || exit 1
+	sudo SSH_AUTH_SOCK=$SSH_AUTH_SOCK SSH_AGENT_PID=$SSH_AGENT_PID rsync --exclude='*root/' --exclude='dev' --exclude='proc' --exclude='var/log' --exclude='mnt' --exclude='var/cache' --exclude='etc' -av ${SRC} ${DEST} || exit 1
 fi
