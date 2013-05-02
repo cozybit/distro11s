7df86b4475ff3fd11963249eb106aae7901d90f5
Javier Cardona <javier@cozybit.com>
2013-05-01 18:20:46 -0700

    Add support for multiple networked qemu instances
    When invoked with an index parameter, qemu.sh will launch a qemu
    instance prepared to run concurrently and network with other qemu
    instances on the same host.  So you should now be able to do
    
     board/qemu/qemu.sh 0
     board/qemu/qemu.sh 1
     board/qemu/qemu.sh 3
    ...
    
    on different shells and get separate qemu instances that can ping each
    other.
    
    For each instance, qemu.sh will now:
    
    1. Create a clone rootfs image
    2. Create a separate tap interface
    3. Set a different MAC address (-device e1000,...,mac=....)
    4. Pick a new port for qemu's gdb server (-gdb tcp:...)
    
    In the first instantiation of an instance you will need to change its IP
    address by editing /etc/distro11s.conf.  The change is persistent, and
    will only need to be done again when you delete the instance's
    out/qemu/rootfs-<index>.cow2 file.
    
    Signed-off-by: Javier Cardona <javier@cozybit.com>

--- board/qemu/qemu.sh
+++ board/qemu/qemu.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+#
+# if numerical argument is given, create a networked qemu instance
+#
 source `dirname $0`/../../scripts/common.sh
 
 [ "${DISTRO11S_BOARD}" != "qemu" ] && \
@@ -6,15 +9,49 @@ source `dirname $0`/../../scripts/common.sh
 
 IFNAME=`sudo tunctl -u ${USER} -b`
 
+[ -n "$1" -a ! -n "${DISTRO11S_BRIDGE}" ] && { echo "Please define DISTRO11S_BRIDGE in distro11s.conf if you need networked qemu instances"; exit 1; }
+
 function die {
 	sudo tunctl -d ${IFNAME}
+
+	[ ! -n "${DISTRO11S_BRIDGE}" ] && exit $1
+	sudo ip link set ${DISTRO11S_BRIDGE} down
+	# HACK: only delete bridge when instance 0 is brought down, which requires
+	# that you halt qemu.sh 0 last
+	[ "${IDX}" -eq 0 ] && sudo brctl delbr ${DISTRO11S_BRIDGE}
 	exit $1
 }
 
-sudo /sbin/ifconfig ${IFNAME} ${DISTRO11S_HOST_IP} up || die 1
-
 KERNEL=${DISTRO11S_OUT}/qemu/bzImage
 ROOTFS=${DISTRO11S_OUT}/qemu/rootfs.ext3
+IDX=0
+
+# Create clone rootfs images if needed
+if [ -n "$1" ]
+then
+	COW2_BASE=rootfs.cow2
+	pushd ${DISTRO11S_OUT}/qemu;
+	[ -f ${COW2_BASE} ] || qemu-img convert `basename ${ROOTFS}` -O qcow2 ${COW2_BASE}
+	COW2_CLONE=${COW2_BASE%.cow2}-$1.cow2
+	[ -f ${COW2_CLONE} ] || qemu-img create -b ${COW2_BASE} -f qcow2 ${COW2_CLONE}
+	ROOTFS=${DISTRO11S_OUT}/qemu/${COW2_CLONE}
+	popd
+	IDX=$1
+fi
+
+# Create bridge
+if [ -n "${DISTRO11S_BRIDGE}" ]
+then
+	# HACK: only create bridge for instance 0, which requires
+	# that you launch qemu.sh 0 before any other
+	[ ${IDX} -eq 0 ] && sudo brctl addbr ${DISTRO11S_BRIDGE}
+	sudo brctl addif ${DISTRO11S_BRIDGE} ${IFNAME}
+	sudo /sbin/ifconfig ${IFNAME} 0.0.0.0 || die 1
+	sudo /sbin/ifconfig ${DISTRO11S_BRIDGE} ${DISTRO11S_HOST_IP} up || die 1
+else
+	sudo /sbin/ifconfig ${IFNAME} ${DISTRO11S_HOST_IP} up || die 1
+fi
+
 
 
 QEMU=qemu-system-`uname -m`
@@ -26,10 +63,10 @@ fi
 ${QEMU} -nographic -kernel ${KERNEL} \
 	-hda ${ROOTFS} \
 	-append "root=/dev/sda combined_mode=ide console=ttyS0" \
-	-device e1000,netdev=lan0 \
+	-device e1000,netdev=lan0,mac=52:54:00:12:34:$((56 + IDX)) \
 	-netdev tap,id=lan0,ifname=$IFNAME,script=no \
 	-enable-kvm -smp 2 \
-	-gdb tcp::1234
+	-gdb tcp::$((1234 + IDX))
 
 # To add a usb device to your qemu build:
 # 1. Make sure you blacklist the module on your host (e.g add blacklist file on 
