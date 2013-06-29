5bfbfc2c76dfc65dc24dc4b81089aca8c223acf6
Colleen Twitty <colleen@cozybit.com>
2013-06-28 18:42:04 -0700

    compat drivers script: cp makefile before grepping

--- scripts/apply-compat-drivers.sh
+++ scripts/apply-compat-drivers.sh
@@ -4,18 +4,23 @@ source `dirname $0`/common.sh
 
 kernel_src=${DISTRO11S_SRC}/kernel
 
-cd ${DISTRO11S_SRC}/compat-drivers-releases
+COMPAT_SRC=${DISTRO11S_SRC}/compat-drivers-releases
+cd ${COMPAT_SRC}
 
 # clean to rebuild
 make clean
 
+cp ${COMPAT_SRC}/drivers/net/wireless/Makefile ${COMPAT_SRC}/drivers/net/wireless/Makefile.old
+
 # only want to build and apply to hwim
-cat drivers/net/wireless/Makefile | grep HWSIM > drivers/net/wireless/Makefile
+cat ${COMPAT_SRC}/drivers/net/wireless/Makefile.old | grep HWSIM > ${COMPAT_SRC}/drivers/net/wireless/Makefile
 
 # build
 do_stamp_cmd compat.make KLIB_BUILD=$kernel_src KLIB=$kernel_src
 # install
 echo 'installing compat drivers'
 sudo make KLIB_BUILD=$kernel_src KLIB=$kernel_src KMODPATH_ARG="INSTALL_MOD_PATH=${STAGING}" install-modules
+
+git checkout ${COMPAT_SRC}/drivers/net/wireless/Makefile
 
 echo 'done installing compat drivers'
