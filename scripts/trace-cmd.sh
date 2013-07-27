5d3f36aba69c400616078d73f78943ec204e09f7
Thomas Pedersen <thomas@cozybit.com>
2013-07-27 16:25:29 -0700

    trace-cmd: do make clean first
    fixes the error:
    
    make: *** No rule to make target `/usr/lib/gcc/x86_64-linux-gnu/4.7/include/stddef.h', needed by `trace-cmd.o'.  Stop.

--- scripts/trace-cmd.sh
+++ scripts/trace-cmd.sh
@@ -5,7 +5,7 @@ source `dirname $0`/common.sh
 [ -d ${STAGING}/src ] || sudo mkdir ${STAGING}/src
 sudo mount --bind ${DISTRO11S_SRC} ${STAGING}/src
 
-echo "cd /src/trace-cmd; make trace-cmd;" > ${STAGING}/trace-cmd.sh
+echo "cd /src/trace-cmd; make clean; make trace-cmd;" > ${STAGING}/trace-cmd.sh
 chmod +x ${STAGING}/trace-cmd.sh
 do_stamp_cmd trace-cmd.make sudo chroot ${STAGING} /trace-cmd.sh
 
