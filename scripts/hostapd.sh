e7ffaa0af1e2f818fde641c3de9b2e61285e6344
Jason Abele <jason@cozybit.com>
2014-04-14 19:02:17 -0700

    build hostapd inside chroot
    hostapd was not built inside the chroot and could therefore link to libs
    on the dev machine instead of those that will be installed on the
    DISTRO11S_BOARD.  Fix this by compiling from within the STAGING chroot.

--- scripts/hostapd.sh
+++ scripts/hostapd.sh
@@ -2,13 +2,20 @@
 
 source `dirname $0`/common.sh
 
-Q pushd ${DISTRO11S_SRC}/hostap/hostapd || exit 1
+[ -d ${STAGING}/src ] || sudo mkdir ${STAGING}/src
+sudo mount --bind ${DISTRO11S_SRC} ${STAGING}/src
 
 CONFIG=${TOP}/board/${DISTRO11S_BOARD}/hostapd_build.config
 if [ ! -e ${CONFIG} ]; then
 	echo "No config for hostapd in ${CONFIG}, using default"
 	CONFIG=defconfig
 fi
-do_stamp_cmd hostapd.config cp ${CONFIG} ./.config && echo "CONFIG_LIBNL32=y" >> ./.config
-do_stamp_cmd hostapd.make "make clean; PREFIX=${STAGING}/usr/local make -j ${DISTRO11S_JOBS};"
-do_stamp_cmd hostapd.install DESTDIR=${STAGING} make install && cp hostapd.conf ${STAGING}/etc/
+
+HOSTAPD_SRC_DIR=${STAGING}/src/hostap/hostapd/
+do_stamp_cmd hostapd.config cp ${CONFIG} ${HOSTAPD_SRC_DIR}/.config && echo "CONFIG_LIBNL32=y" >> ${HOSTAPD_SRC_DIR}/.config
+HOSTAPD_MAKE_CMD="'cd /src/hostap/hostapd; make clean; CFLAGS=-I/usr/local/include/libnl3 make -j 2'"
+do_stamp_cmd hostapd.make "sudo chroot ${STAGING} /bin/bash -c ${HOSTAPD_MAKE_CMD}"
+HOSTAPD_INSTALL_CMD="'cd /src/hostap/hostapd; make install && cp hostapd.conf /etc/'"
+do_stamp_cmd hostapd.install "sudo chroot ${STAGING} /bin/bash -c ${HOSTAPD_INSTALL_CMD}"
+
+sudo umount ${STAGING}/src
